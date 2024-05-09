b423f91651104029027ab076de580e129f4d15e3
HaseefMuhammed <haseefmuhammedpc@gmail.com>
2024-05-09 22:56:14 +0530

    bugs and animations

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Function to handle 404 redirection
-    function redirectTo404() {
-        window.location.href = "/404.html"; // Update the path as needed
-    }
+ScrollReveal().reveal('.card', {
+    scale: 0.85
+});
 
-    // Check if the URL is not the expected one and redirect to 404
-    if (window.location.href !== "https://haseefmuhammed.github.io/WebCraft/") {
-        redirectTo404();
+ScrollReveal().reveal('.flip', {
+    rotate: {
+        x: 20,
+        z: 20
     }
 });
+
+ScrollReveal().reveal('.scaleUp', { scale: 0.85 });
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     // Function to handle 404 redirection
+//     function redirectTo404() {
+//         window.location.href = "/404.html"; // Update the path as needed
+//     }
+
+//     // Check if the URL is not the expected one and redirect to 404
+//     if (window.location.href !== "http://webcraaft.42web.io") {
+//         redirectTo404();
+//     }
+// });
